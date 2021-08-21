ba882d14c968129df136f65c19b5406cad8a4e8b
Hung Bang <hung.bang@evizi.com>
2021-08-21 20:43:24 +0700

    - Initial spring boot project

---
+++ src/main/java/com/bang/springbootawsdocdb/SpringBootAwsDocdbApplication.java
@@ -0,0 +1,42 @@
+package com.bang.springbootawsdocdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAwsDocdbApplication {
+
+    public static void main(String[] args) {
+        SSLContextHelper.setSslProperties();
+        SpringApplication.run(SpringBootAwsDocdbApplication.class, args);
+    }
+
+    protected static class SSLContextHelper {
+        private static final Logger logger = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
+        private static final String SSL_TRUST_STORE = "javax.net.ssl.trustStore";
+        private static final String SSL_TRUST_STORE_PASSWORD = "javax.net.ssl.trustStorePassword";
+        private static final String SSL_TRUST_STORE_TYPE = "javax.net.ssl.trustStoreType";
+        private static final String KEY_STORE_TYPE = "JKS";
+        private static final String DEFAULT_KEY_STORE_PASSWORD = "changeit";
+        private static final String DEFAULT_KEYSTORE = "rds-truststore.jks";
+        private static final String SSL_KEYSTORE = "sslKeyStore";
+
+
+        private static void setSslProperties() {
+            try {
+                String sslKeyStore= System.getProperty(SSL_KEYSTORE);
+                logger.info("-DsslKeyStore={}", sslKeyStore);
+                if(StringUtils.isEmpty(sslKeyStore)) {
+                    sslKeyStore= DEFAULT_KEYSTORE;
+                }
+                logger.info("ssl keystore path: {}", sslKeyStore);
+                System.setProperty(SSL_TRUST_STORE, sslKeyStore);
+                System.setProperty(SSL_TRUST_STORE_TYPE, KEY_STORE_TYPE);
+                System.setProperty(SSL_TRUST_STORE_PASSWORD, DEFAULT_KEY_STORE_PASSWORD);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+}
